7488bdb81e0ff800153051bbb97bfc41973ce848
Hp <Hp@LAPTOP-H4UM03GO>
2023-03-20 12:41:41 +0530

    old programs added

---
+++ trainingcode/program1.java
@@ -0,0 +1,21 @@
+package trainingcode;
+/**
+ * 
+ */
+
+/**
+ * @author Hp
+ *
+ */
+public class program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
